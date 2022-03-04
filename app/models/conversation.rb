ed566b31215f1941d31651f7bd776324da2fc189
Anthony <tonymich2012@gmail.com>
2022-03-04 11:17:36 -0800

    adds Model Associations.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,5 @@
 class Conversation < ApplicationRecord
+  belongs_to: recipiant #What do I do 
+  belongs_to: sender #What do with is as well
+  has_manys: messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many: pets
+  has_many: messages
+  has_many: conversations
 end
