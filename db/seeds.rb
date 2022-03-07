e3a8de195b8c669a6424b00397a9bf0fae1b5794
Anthony <tonymich2012@gmail.com>
2022-03-06 18:37:16 -0800

    adds basic/test user and pet seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!([
-  {{
-    
-  }}
-])
+User.create!(name: "test1", email: "one@test.com", password: "password")
+User.create!(name: "test2", email: "two@test.com", password: "password")
+
+Pet.create!(name: "Turkey", gender: "male", description: "A tabby cat that likes to do nothing more than to annoy his owner and beg for attention. He loves to sleep and cuddle ontop of people.", img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-sLgf6bCvUFxwQ_5TnXxSDeK-5dug_kP5Nw&usqp=CAU", likes: "eating, sleeping", dislikes: "loud noises, dogs" fixed: true)
+Pet.create!(name: "fatsy", gender: "male", description: "A grey cat that likes to do nothing more than to eat sleep and scracth the corners on objects.", img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-sLgf6bCvUFxwQ_5TnXxSDeK-5dug_kP5Nw&usqp=CAU", likes: "eating, sleeping", dislikes: "loud noises, diet food" fixed: true)
