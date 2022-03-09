171a14b8cf89793a40b21031b5c46f7c180c6134
Anthony <tonymich2012@gmail.com>
2022-03-08 19:19:40 -0800

    add's user methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  # This is a user signup in essence.
   def create
     user = User.new(
       name: params[:name],
       email: params[:email],
+      location: params[:location],
+      phone_number: params[:phone_number],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
@@ -11,5 +14,29 @@ class UsersController < ApplicationController
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
+  end
+end
+
+def show
+  user = User.find_by(id: params[:id])
+  render json: user
+end
+#this allows a user to update their user id. Do I need a User update?
+  def update 
+    user = User.find_by(id: params[:id])
+    user.name = params[:name] || user.name
+    user.location = params[:location] || user.location
+    user.phone_number = params[:phone_number] || user.phone_number
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+#this allows the user to deletes one of their user "listings/posts"
+  def destroy 
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: { message: "User successfully destroyed!" }
   end
 end
